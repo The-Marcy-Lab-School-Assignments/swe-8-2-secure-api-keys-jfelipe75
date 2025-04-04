5dfdfc0a63ddab547dc9a9a42a83fa5c2ff45d8e
Juan Felipe Garcia <jfelipegarcia23@gmail.com>
2025-04-04 16:10:03 -0400

    completing the bonus task

--- server/index.js
+++ server/index.js
@@ -2,27 +2,48 @@
 // Imports
 //////////////////////////
 
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
+require("dotenv").config();
+const { error } = require("console");
 
 //////////////////////////
 // Constants
 //////////////////////////
 
 const port = 8080;
-const pathToDistFolder = path.join(__dirname, '../frontend/dist');
+const pathToDistFolder = path.join(__dirname, "../frontend/dist");
 const app = express();
 
 //////////////////////////
 // Middleware/Controllers
 //////////////////////////
+const serveTrendingGifs = async (req, res) => {
+  const url = `https://api.giphy.com/v1/gifs/trending?limit=3&rating=g&api_key=${process.env.API_KEY}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.send(data); // send back the Giphy response
+  } catch (error) {
+    console.error("Error fetching Giphy data:", error.message);
+    res.status(503).send({ error: "Unable to fetch trending gifs" });
+  }
+};
+
+const serverSearchedGifs = async (req, res) => {
+  const search = req.query;
+};
 
 const serveStatic = express.static(pathToDistFolder);
 
 app.use(serveStatic);
 
+// Routes
+app.get("/api/gifs", serveTrendingGifs);
+
 //////////////////////////
 // Listener
 //////////////////////////
 
-app.listen(port, () => console.log(`listening at http://localhost:${port}`)); 
+app.listen(port, () => console.log(`listening at http://localhost:${port}`));
