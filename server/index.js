28e96dce88b931262245deb5a6620d27858dc812
Juan Felipe Garcia <jfelipegarcia23@gmail.com>
2025-04-06 22:34:29 -0400

    trying again

--- server/index.js
+++ server/index.js
@@ -5,12 +5,8 @@
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
-import { fileURLToPath } from "url";
 
 dotenv.config();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 //////////////////////////
 // Constants
